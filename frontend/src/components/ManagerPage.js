a346a6309489fd1e5214877e60f08a011a2be962
Alexandru Ariton <aritonalexandru41@yahoo.com>
2024-01-12 00:52:02 +0200

    modificare pagina Manager

--- frontend/src/components/ManagerPage.js
+++ frontend/src/components/ManagerPage.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './ManagerPage.css';
+
 
 const ManagerPage = () => {
   const [tasks, setTasks] = useState([]); // Inițializăm starea pentru task-uri
-
+  const [users, setUsers] = useState([]); // State for users
   // Funcția pentru adăugarea unui nou task
   const handleAddTaskClick = () => {
     const taskName = prompt('Introdu numele task-ului nou:');
@@ -18,20 +20,34 @@ const ManagerPage = () => {
       localStorage.setItem('tasks', JSON.stringify(updatedTasks)); // Salvăm în LocalStorage
     }
   };
-
+ 
   // La încărcarea componentei, încarcă task-urile salvate din LocalStorage
   useEffect(() => {
     const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('/api/users'); // Replace with your API endpoint
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    }
     if (savedTasks) {
       setTasks(savedTasks);
     }
+    fetchUsers();
+
   }, []);
 
   return (
     <div className="manager-page">
       <aside className="user-list">
         <h3>Utilizatori</h3>
-        {/* Lista utilizatorilor */}
+        <ul>
+          {users.map(user => (
+            <li key={user.user_id}>{user.user_name}</li>
+          ))}
+        </ul>
       </aside>
       <section className="tasks-section">
         <h3>Task-uri</h3>
